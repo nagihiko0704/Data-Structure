2ddeba898476676c26966a8e7d41615c742a9d01
LeeSangHwa593 <37610743+LeeSangHwa593@users.noreply.github.com>
2018-04-18 00:24:27 +0900

    Add files via upload

---
+++ 연습용.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef int element
+
+//스택의 노드를 위한 구조체 및 linkedstack 관련 구조체
+
+typedef struct stacknode {
+	element item;
+	struct stacknode * link;
+} stacknode;
+
+
+//스택의 가장 위를 가리킴. 
+
+typedef struct {
+	stacknode * top;
+}linkedstacktype;
+
+//초기화 함수
+void init(linkedstacktype * s) {
+
+	s->top = NULL;
+};
+
+
+
+//push 연산 
+
+void push(linkedstacktype*s, element item) {
+	stacknode * temp = (stacknode*)malloc(sizeof(stacknode));
+
+	if (temp == NULL) {
+		printf("에러");
+
+		return;
+	}
+
+	else {
+		temp->item = item;
+		temp->link = s->top;
+		s->top = temp;
+	}
+}
+
+//POP연산
+
+element pop(linkedstacktype * s) {
+
+	if (is_empty(s)) {
+		printf("에러");
+		exit(1);
+	}
+
+	else {
+		stacknode * temp = s->top;
+		int item = temp->item;
+		s->top = s->top->link;
+		free(temp);
+		return item;
+	}
+}
+
+//  좌표 저장용 구조체 선언
+typedef struct {
+
+	int row;
+	int column;
+
+}pos;
+
+void push_loc()
+
+int main(void) {
+
+
+	//2차원 배열 미로 선언
+	int maze[10][10] = {
+		
+						{1,1,1,1,1,1,1,1,1,1},
+						{0,0,0,0,1,0,0,0,0,1},
+						{1,0,0,0,1,0,0,0,0,1},
+						{1,0,1,1,1,0,0,1,0,1},
+						{1,0,0,0,1,0,0,1,0,1},
+						{1,0,1,0,1,0,0,1,0,1},
+						{1,0,1,0,1,0,0,1,0,1},
+						{1,0,1,0,1,0,0,1,0,1},
+						{1,0,1,0,0,0,0,1,0,0},
+						{1,1,1,1,1,1,1,1,1,1}
+	};
+	
+
+
+
+
+
+	return 0;
+
+}
