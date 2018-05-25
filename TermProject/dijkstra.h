0171e3bbc21fb57ef89251be9e786a9b20ea8205
tata42 <fldjzksms@gmail.com>
2018-05-25 22:40:26 +0900

    Modify constructure of Heap

--- TermProject/dijkstra.h
+++ TermProject/dijkstra.h
@@ -2,17 +2,10 @@
 
 typedef struct
 {
-	int dist;	// 시작점으로부터의 거리
-	int num;	// 역 번호
-	int short_station;	//가장 가까운 역
-}HeapNode;
-
-typedef struct
-{
-	HeapNode node[MAX_NODE];
+	StationNode node[MAX_NODE];
 	int node_num;
 }Heap;
 
 void init_heap(Heap *h);
-void insert_node_min_heap(Heap *h, HeapNode node);
-HeapNode delete_node_min_heap(Heap *h);
+void insert_node_min_heap(Heap *h, StationNode node);
+StationNode delete_node_min_heap(Heap *h);
