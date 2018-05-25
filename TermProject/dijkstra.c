ab99c5b3a99796ecea5b2adb392b20e7ad28c8b5
tata42 <fldjzksms@gmail.com>
2018-05-25 22:41:41 +0900

    Fix functions

--- TermProject/dijkstra.c
+++ TermProject/dijkstra.c
@@ -4,49 +4,56 @@ void init_heap(Heap *h)
 {
 	h->node_num = 0;
 	for (int i = 0; i < MAX_NODE; i++) {
-		h->node[i].dist = INF;
+		h->node[i].dis = INF;
 	}
 }
 
-void insert_node_min_heap(Heap *h, HeapNode node)
+/*
+min heap에 노드 삽입 key는 거리
+*/
+void insert_node_min_heap(Heap *h, StationNode node)
 {
-	int i = ++(h->node_num);
+	int index = ++(h->node_num);
 
-	while (i != 1 && (node.dist < h->node[i / 2].dist))
-	{
-		h->node[i] = h->node[i / 2];
-		i /= 2;
-	}
+    //맨 마지막 위치에서 시작해서 삽입하려는 노드가 부모 노드보다 작으면 위로 거슬러 올라감
+    for (int index = ++(h->node_num); index != 1 && (node.dis < h->node[index / 2].dis); index /= 2)
+    {
+        h->node[index] = h->node[index / 2];
+    }
 
-	h->node[i] = node;
+	h->node[index] = node;
 }
 
-HeapNode delete_node_min_heap(Heap *h)
+/*
+min heap의 루트 노드 삭제
+*/
+StationNode delete_node_min_heap(Heap *h)
 {
 	int parent, child;
-	HeapNode node, tmp;
+	StationNode node, temp;
 
-	node = h->node[1];
-	tmp = h->node[h->node_num--];
-	h->node[1] = tmp;
+	node = h->node[1];              //반환할 루트 노드
+	temp = h->node[h->node_num--];  //맨 마지막 노드
+	h->node[1] = temp;
 	parent = 1;
 	child = 2;
 
 	while (child <= h->node_num)
 	{
-		// 자식 노드 중 작은 걸 선택
-		if (child < h->node_num && (h->node[child].dist > h->node[child + 1].dist))
+		//자식 노드 중 작은 걸 선택
+		if (child < h->node_num && (h->node[child].dis > h->node[child + 1].dis))
 		{
 			child++;
 		}
-		// 자식 노드가 더 크면 중지
-		if (tmp.dist <= h->node[child].dist) break;
 
-		h->node[parent] = h->node[child];
+        //현재 비교하고 있는 노드보다 맨 마지막 노드가 더 작으면 빠져나감
+		if (temp.dis <= h->node[child].dis) break;
+
+		h->node[parent] = h->node[child];   //삭제된 자리에 자식노드 삽입
 		parent = child;
-		child *= 2;
+		child *= 2;     //밑 세대로 감
 	}
-	h->node[parent] = tmp;
+	h->node[parent] = temp; //삭제된 자리에 맨 마지막 노드 삽입
 
 	return node;
 }
