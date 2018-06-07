eb47135a1ea85840e9ce9cdfa9e4941f80a219fa
tata42 <fldjzksms@gmail.com>
2018-06-07 21:24:46 +0900

    Rename struct

--- TermProject/user.h
+++ TermProject/user.h
@@ -1,19 +1,19 @@
 #pragma once
 #define MAX_USER 7500
 
-typedef struct word //트윗 구조체
+typedef struct TweetType //트윗 구조체
 {
     char upload_date[40];   //올린 날짜
     char tweet[270];        //내용
-    struct word *next_tweet;//다른 트윗
-}Word;
+    struct TweetType *next_tweet;//다른 트윗
+}TweetType;
 
 typedef struct user //유저 정보
 {
     int id_num;					//고유번호
     char sign_up_date[40];      //가입일
     char name[20];				//닉네임
-    Word *tweets;				//트윗
+    TweetType *tweets;				//트윗
     struct user *friends;		//친구
 }User;
 
